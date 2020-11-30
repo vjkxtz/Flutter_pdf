27f6c54560198f92e13375e6a197691c9869ca1d
vijaykrish <32586705+vjkxtz@users.noreply.github.com>
2020-11-30 22:49:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:manuals/homepage.dart';
 void main(){
   runApp(MaterialApp(
     home: mainpage(),
-    theme: ThemeData.light(),
+    theme: ThemeData.light().copyWith(
+      primaryColor: Color(0xFF0A0E21),
+      scaffoldBackgroundColor: Color(0xFF0A0E21)
+    ),
   ));
 }
